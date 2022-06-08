0fddb52c4d54cae8b5c56de279397fda93624a2e
joonmarion <marianojoao52@gmail.com>
2022-06-08 09:05:23 -0300

    Aula 4

---
+++ Curso-JavaScript/Aula-4-Operadores/scripts.js
@@ -0,0 +1,32 @@
+function comparaNumeros(num1, num2) {
+    const primeiraFrase = saoIguais(num1, num2);
+    const segundaFrase = somaNums(num1, num2);
+    return `${primeiraFrase} ${segundaFrase}`;
+}
+
+function saoIguais(num1, num2) {
+    let confirm = 'não ';
+
+    if (num1 == num2) {
+        confirm = '';
+    }
+
+    return `Os números ${num1} e ${num2} ${confirm}são iguais.`;
+}
+
+function somaNums(num1, num2) {
+    const soma = num1 + num2;
+    let comparaDez = 'menor';
+    let comparaVinte = 'menor';
+
+    if (soma > 10) {
+        comparaDez = 'maior';
+    }
+    if (soma > 20) {
+        comparaVinte = 'maior';
+    }
+
+    return `Sua soma é ${soma}, que é ${comparaDez} que 10 e ${comparaVinte} que 20.`;
+}
+
+console.log(comparaNumeros(10, 9));
