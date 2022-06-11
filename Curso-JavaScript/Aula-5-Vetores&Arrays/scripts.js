101094e594c959961da17f95cf9bb7471417a528
joonmarion <marianojoao52@gmail.com>
2022-06-11 11:03:15 -0300

    Aula 5

--- Curso-JavaScript/Aula-5-Vetores&Arrays/scripts.js
+++ Curso-JavaScript/Aula-5-Vetores&Arrays/scripts.js
@@ -1,42 +1,42 @@
 let array = [1, 2, true, 'string'];
 let array2 = [1, 2, true, 'string', ['array2', 'array3', ['teste', 2]], ['array4']];
 
-// PEGANDO ÍNDICE DENTRO DO ARRAY
+/* PEGANDO ÍNDICE DENTRO DO ARRAY*/
 console.log(array2[4][2][0]);
 
-// PERCORRE TODO O ARRAY
+/* PERCORRE TODO O ARRAY */
 array2.forEach(function (item, index) {
     console.log(item, index);
 });
 
-// ADICIONA ITEM AO FINAL DO ARRAY
+/* ADICIONA ITEM AO FINAL DO ARRAY */
 array.push('New item');
 console.log(array);
 
-// RETIRA O ÚLTIMO ITEM DO ARRAY
+/* RETIRA O ÚLTIMO ITEM DO ARRAY */
 array.pop(array);
 console.log(array);
 
-// RETIRA O PRIMEIRO ITEM DO ARRAY
+/* RETIRA O PRIMEIRO ITEM DO ARRAY */
 array.shift(array);
 console.log(array);
 
-// ADICIONA ITEM NO INICIO DO ARRAY
+/* ADICIONA ITEM NO INICIO DO ARRAY */
 array.unshift('New start item');
 console.log(array);
 
-// INDEX DO VALOR NO ARRAY
+/* INDEX DO VALOR NO ARRAY */
 console.log(array2[4][2].indexOf('teste'));
 
-// DELETANDO ITEM PELO ÍNDICE
+/* DELETANDO ITEM PELO ÍNDICE */
 array.splice(0, 2);
 console.log(array);
 
-// RETORNANDO ALGUNS ELEMENTOS DO ARRAY PELO ÍNDICE
+/* RETORNANDO ALGUNS ELEMENTOS DO ARRAY PELO ÍNDICE */
 const sliceArray = array2.slice(1, 4);
 console.log(sliceArray);
 
-// OBJETOS
+/* OBJETOS */
 let object = {
     string: 'string2',
     number: 1,
@@ -51,3 +51,10 @@ console.log(object.number);
 
 var string = object.string;
 console.log(string);
+
+/* ARRAY WITH ARGUMENT */
+const arrayWithArgument = (...args) => {
+    return console.log(args);
+};
+
+arrayWithArgument(1, 'dois', 3);
