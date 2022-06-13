fedb94753dfa83661aa2656290414d92fc160ab8
joonmarion <marianojoao52@gmail.com>
2022-06-13 10:00:41 -0300

    Aula 10

--- Curso-JavaScript/Aula-10-Objetos/scripts.js
+++ Curso-JavaScript/Aula-10-Objetos/scripts.js
@@ -1,4 +1,4 @@
-// ATRIBUINDO SEPARADAMENTE
+/* ATRIBUINDO SEPARADAMENTE */
 let obj = {};
 
 obj.name = 'Mariano';
@@ -8,7 +8,7 @@ obj.adress = 'Rua 8';
 console.log(obj);
 console.log(Object.keys(obj));
 
-// ATRIBUINDO NA CRIAÇÃO
+/* ATRIBUINDO NA CRIAÇÃO */
 let obj2 = {
     name: 'João',
     age: 21,
@@ -18,6 +18,27 @@ let obj2 = {
 console.log(obj2);
 console.log(Object.values(obj2));
 
-// NOVA KEY EM obj2
+/* NOVA KEY EM obj2 */
 obj2['numberOfSiblings'] = 3;
 console.log(obj2);
+
+/* object destructuring */
+const user = {
+    id: 42,
+    displayName: 'Paulo',
+    fullName: {
+        firstName: 'Paulo',
+        lastName: 'Silva',
+    },
+};
+
+function userId({ id }) {
+    return id;
+}
+
+function getFullName({ fullName: { firstName: first, lastName: last } }) {
+    return `${first} ${last}`;
+}
+
+console.log(userId(user));
+console.log(getFullName(user));
